20bc2f9efbbc0c224a13b46e3f3d08f12b0092d6
MahouShoujoMivutilde <MahouShoujoMivutilde@users.noreply.github.com>
2017-12-30 09:51:44 +0300

    init

---
+++ dingo_tray/kindof_console.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace dingo_tray
+{
+    partial class kindof_console
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kindof_console));
+            this.tray_icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.dingo_output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tray_icon
+            // 
+            this.tray_icon.Icon = ((System.Drawing.Icon)(resources.GetObject("tray_icon.Icon")));
+            this.tray_icon.Text = "Dingo DNS";
+            this.tray_icon.Visible = true;
+            this.tray_icon.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // dingo_output
+            // 
+            this.dingo_output.BackColor = System.Drawing.Color.Black;
+            this.dingo_output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dingo_output.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dingo_output.ForeColor = System.Drawing.Color.White;
+            this.dingo_output.Location = new System.Drawing.Point(0, 0);
+            this.dingo_output.Name = "dingo_output";
+            this.dingo_output.ReadOnly = true;
+            this.dingo_output.Size = new System.Drawing.Size(918, 441);
+            this.dingo_output.TabIndex = 0;
+            this.dingo_output.Text = "";
+            // 
+            // kindof_console
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(918, 441);
+            this.Controls.Add(this.dingo_output);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "kindof_console";
+            this.Opacity = 0.4D;
+            this.Text = "Dingo DNS";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.kindof_console_FormClosed);
+            this.SizeChanged += new System.EventHandler(this.kindof_console_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon tray_icon;
+        private System.Windows.Forms.RichTextBox dingo_output;
+    }
+}
+
---
+++ dingo_tray/kindof_console.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace dingo_tray
+{
+    public partial class kindof_console : Form
+    {
+        int dingo_id;
+        string dingo_defaults = "-port 53 -gdns:server 216.58.209.142 -h1";
+
+        public kindof_console()
+        {
+            InitializeComponent();
+            dingo_id = RunWithRedirect("dingo.exe", GetArguments(), dingo_output);
+
+            StartPosition = FormStartPosition.Manual;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+
+            ShowInTaskbar = false;
+        }
+
+        private string GetArguments() {
+            string path = "dingo_tray.conf";
+            if (!File.Exists(path))
+            {
+                tray_icon.ShowBalloonTip(5000, "Dingo DNS", "Не найден " + path + ", запуск dingo.exe с аргментами по умолчанию: " + dingo_defaults, ToolTipIcon.Info);
+                return dingo_defaults;
+            } else
+            {
+                using (StreamReader sr = new StreamReader("dingo_tray.conf"))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void kindof_console_SizeChanged(object sender, EventArgs e)
+        { 
+            Visible = WindowState != FormWindowState.Minimized;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Visible = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        // https://stackoverflow.com/a/415655
+        private int RunWithRedirect(string cmdPath, string args, RichTextBox output)
+        {
+            if (!File.Exists(cmdPath))
+            {
+                tray_icon.ShowBalloonTip(10000, "Dingo DNS", "Не найден " + cmdPath + ", приложение, с надеждой в сердце и трепетным взглядом в будущее, закроется через 10 секунд...", ToolTipIcon.Error);
+                Thread.Sleep(10000);
+                Environment.Exit(1);
+                return -1;
+            }
+            else
+            {
+
+                void proc_DataReceived(object sender, DataReceivedEventArgs e)
+                {
+                    if (!String.IsNullOrEmpty(e.Data)) ThreadSafeAppendText(output, e.Data + "\n");
+                }
+
+                var proc = new Process();
+                proc.StartInfo.FileName = cmdPath;
+                proc.StartInfo.Arguments = args;
+
+                // set up output redirection
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.EnableRaisingEvents = true;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = false;
+
+                // see below for output handler
+                proc.ErrorDataReceived += proc_DataReceived;
+                proc.OutputDataReceived += proc_DataReceived;
+
+                proc.Start();
+
+                proc.BeginErrorReadLine();
+                proc.BeginOutputReadLine();
+
+                return proc.Id;
+            }
+        }
+
+        private void ThreadSafeAppendText(RichTextBox rtb, string text)
+        {
+            Invoke((MethodInvoker)delegate ()
+            {
+               rtb.AppendText(text);
+            });
+        }
+
+        private void kindof_console_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try {
+                Process.GetProcessById(dingo_id).Kill();
+            }
+            catch { }
+        }
+    }
+}
